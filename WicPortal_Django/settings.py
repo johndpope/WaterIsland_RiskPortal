7f40f8fa59d8958e2fc5fe9032ada456c1f21035
kgorde <kgorde@wicfunds.com>
2019-02-08 09:22:35 -0500

    Moved to Development Branch. Resolved dependency between Dev and Prod. Pandas

--- WicPortal_Django/settings.py
+++ WicPortal_Django/settings.py
@@ -237,4 +237,4 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
