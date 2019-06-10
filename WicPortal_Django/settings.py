f81a5ae563578f6412564196a796c5fa3294dc1a
kgorde <37445671+kgorde@users.noreply.github.com>
2019-06-10 10:01:05 -0400

    Moved ESS Implied Probabilities Refresh to 1pm (#169)

--- WicPortal_Django/settings.py
+++ WicPortal_Django/settings.py
@@ -96,7 +96,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'REFRESH_ESS_LONG_SHORT_AND_IMPLIED_PROB': {
         'task': 'portfolio_optimization.tasks.refresh_ess_long_shorts_and_implied_probability',
-        'schedule': crontab(minute="15", hour='8', day_of_week='mon-fri'),
+        'schedule': crontab(minute="00", hour='13', day_of_week='mon-fri'),
     },
     'REFRESH_CREDIT_DEALS_UPSIDE_DOWNSIDE': {
         'task': 'risk_reporting.tasks.refresh_credit_deals_upside_downside',
