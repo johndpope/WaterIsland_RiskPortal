e534aec5f93641e7d1960e92ad86ecd48d5d8a22
kgorde <kgorde@wicfunds.com>
2019-06-11 09:58:20 -0400

    Changed Date format for ESS Potential Long/Shorts

--- static/app-assets/js/core/implied_probability_track.js
+++ static/app-assets/js/core/implied_probability_track.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     let implied_probability_chart = $.parseJSON($('#implied_probability_chart').val());
     let field_names = $.parseJSON($('#field_names').val());
 
     let title = "Implied Probability Tracker";
-    let fieldMappingsArray = ['AED Long', 'AED Short','N/A', 'Post Re-org Equity', 'Soft Universe Imp. Prob',
-        'Spec M&A','Spin-Off','Stub Value','TAQ Long','TAQ Short','Transformational M&A', 'Turnaround',
+    let fieldMappingsArray = ['AED Long', 'AED Short', 'N/A', 'Post Re-org Equity', 'Soft Universe Imp. Prob',
+        'Spec M&A', 'Spin-Off', 'Stub Value', 'TAQ Long', 'TAQ Short', 'Transformational M&A', 'Turnaround',
         'Universe (Long)', 'Universe (Short)', 'Universe (Unclassified)'];
 
     let datasets = createDataSets(implied_probability_chart, field_names, fieldMappingsArray, false, fieldMappingsArray);
@@ -13,38 +13,43 @@ $(document).ready(function(){
     console.log(datasets);
     let graphs = [];
     let counter = 1;
-    let is_hidden=true;
-    for(var i in fieldMappingsArray){
-        if(fieldMappingsArray[i] === 'Universe (Long)' || fieldMappingsArray[i] === 'Universe (Short)' || fieldMappingsArray[i] === 'Universe (Unclassified)'){
+    let is_hidden = true;
+    for (var i in fieldMappingsArray) {
+        if (fieldMappingsArray[i] === 'Universe (Long)' || fieldMappingsArray[i] === 'Universe (Short)' || fieldMappingsArray[i] === 'Universe (Unclassified)') {
             is_hidden = false;
         }
-        else{
+        else {
             is_hidden = true;
         }
-         graphs.push({
-                "useDataSetColors": false,
-                "id": "g" + counter++,
-                "hidden": is_hidden,
-                "valueField": fieldMappingsArray[i],
-                "valueAxis": "a1",
-                "title": fieldMappingsArray[i].toString(),
-                "lineThickness": 1.5,
-                "lineColor": randomColor({
-                    luminosity: 'bright',
-                    hue: 'random',
-                }),
-         });
+        graphs.push({
+            "useDataSetColors": false,
+            "id": "g" + counter++,
+            "hidden": is_hidden,
+            "valueField": fieldMappingsArray[i],
+            "valueAxis": "a1",
+            "title": fieldMappingsArray[i].toString(),
+            "lineThickness": 1.5,
+            "lineColor": randomColor({
+                luminosity: 'bright',
+                hue: 'random',
+            }),
+        });
     }
 
     let probability_tracker = AmCharts.makeChart("implied_probability_track", createLineChartConfigs(implied_probability_chart, datasets, graphs, title, '%', 'black'));
 
     $('#ess_implied_probability_table').DataTable({
-        order: [[0, 'desc']],
+        order: [[0, 'asc']],
         columnDefs: [{
             targets: [2], render: function (data) {
                 return parseFloat(data).toFixed(2).toString() + " %"
             }
-        }],
+        },
+            {
+                targets: [0], render: function (data) {
+                    return moment(data).format('YYYY-MM-DD');
+                }
+            }],
         "pageLength": 100,
         dom: '<"row"<"col-sm-6"Bl><"col-sm-6"f>>' +
             '<"row"<"col-sm-12"<"table-responsive"tr>>>' +
