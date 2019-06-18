44403e4e18552296420a3d6a18282150c7335152
kgorde <37445671+kgorde@users.noreply.github.com>
2019-06-18 16:47:02 -0400

    ESS Long/Short Sorting Refreshed (#190)

--- static/app-assets/js/core/ess_potential_long_short.js
+++ static/app-assets/js/core/ess_potential_long_short.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#ess_ls').DataTable({
         "pageLength": 100,
-        "order":[[0, 'desc']],
+        "order":[[0, 'desc'], [1, 'asc']],
         "columnDefs": [{
             "targets": [1, 2, 3, 6, 7, 8, 9, 10, 11],
             "render": $.fn.dataTable.render.number(',', '.', 2),
