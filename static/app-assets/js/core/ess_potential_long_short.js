71c3c96cdfbb7395193c50b3be25358c46d9f6e3
vaggarwalwic <48919690+vaggarwalwic@users.noreply.github.com>
2019-06-28 12:55:12 -0400

    Add Export as CSV option on long shorts page, add Model WIC column (#222)

--- static/app-assets/js/core/ess_potential_long_short.js
+++ static/app-assets/js/core/ess_potential_long_short.js
@@ -4,9 +4,34 @@ $(document).ready(function () {
         "pageLength": 100,
         "order":[[1, 'asc']],
         "columnDefs": [{
-            "targets": [2, 3, 4, 9, 10, 11, 13, 14, 15, 16, 17, 18],
+            "targets": [2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
             "render": $.fn.dataTable.render.number(',', '.', 2),
         }],
+        dom: '<"row"<"col-sm-6"Bl><"col-sm-6"f>>' +
+            '<"row"<"col-sm-12"<"table-responsive"tr>>>' +
+            '<"row"<"col-sm-5"i><"col-sm-7"p>>',
+        buttons: {
+            buttons: [{
+                extend: 'csv',
+                text: '<i class="fa fa-print"></i> Import as CSV',
+                className: 'btn btn-default btn-xs',
+                customize: function (csv) {
+                    var csvRows = csv.split('\n');
+                    csvRows[0] = 'As Of, Alpha Ticker, PX, Model Up, Model WIC, Model Down, Catalyst, Rating, Deal Type,' +
+                                 'Hedges?, Implied Prob, Return/Risk, Gross IRR, Days To Close, Ann. IRR, Adj. Ann IRR,' +
+                                 'Long Prob, Long IRR, Short Prob, Short IRR, Potential Long, Potential Short'
+                    return csvRows.join('\n');
+                }
+            }],
+            dom: {
+                container: {
+                    className: 'dt-buttons'
+                },
+                button: {
+                    className: 'btn btn-default'
+                }
+            }
+        },
         initComplete: function () {
             this.api().columns([0]).every( function () {
                 var column = this;
