448ac00ad9b56962fc2a023ed94bc203624f5761
kgorde <37445671+kgorde@users.noreply.github.com>
2019-07-16 10:51:14 -0400

    Hard optimization (#239)
    * Fixed foreign name prices
    
    * fixed pnl impact pricing issue

--- portfolio_optimization/portfolio_optimization_utils.py
+++ portfolio_optimization/portfolio_optimization_utils.py
@@ -4,16 +4,16 @@ def calculate_pl_sec_impact(row):
         return 0    # Assume 0 pnl impact on Equity Hedges. Cant say for sure where acquirer would trade
 
     if row['SecType'] != 'EXCHOPT':
-        return (row['deal_value'] * row['FxFactor'] * row['QTY']) - (row['CurrentMktVal'] * row['FxFactor'])
+        return (row['deal_value'] * row['QTY']) - (row['CurrentMktVal'] / row['FxFactor'])
 
     if row['PutCall'] == 'CALL':
         if row['StrikePrice'] <= row['deal_value']:
-            x = (row['deal_value'] - row['StrikePrice']) * (row['QTY']) * row['FxFactor']
+            x = (row['deal_value'] - row['StrikePrice']) * (row['QTY'])
         else:
             x = 0
     elif row['PutCall'] == 'PUT':
         if row['StrikePrice'] >= row['deal_value']:
-            x = (row['StrikePrice'] - row['deal_value']) * (row['QTY']) * row['FxFactor']
+            x = (row['StrikePrice'] - row['deal_value']) * (row['QTY'])
         else:
             x = 0
     return -row['CurrentMktVal'] + x
